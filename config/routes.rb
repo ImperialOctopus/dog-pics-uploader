fe94319ef9729e95f09683066b0fe12ff46d5b3f
Zoey Child <imperialoctopus@gmail.com>
2021-02-01 12:48:16 +0000

    Require clearance sign in to view resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,35 @@ Rails
     # get 'submissions/new'
     root 'static_pages#index'
 
-    resources :submissions
+    # Submissions routing
+
+    resources :submissions, only: %i[new create]
+
+    constraints Clearance::Constraints::SignedIn.new do
+      resources :submissions
+    end
+
+    # Clearance routing
+    resources :passwords,
+              controller: 'clearance/passwords',
+              only: %i[create new]
+
+    resource :session, controller: 'clearance/sessions', only: [:create]
+
+    resources :users,
+              controller: 'clearance/users',
+              only: Clearance.configuration.user_actions do
+      resource :password,
+               controller: 'clearance/passwords',
+               only: %i[edit update]
+    end
+
+    get '/admin', to: redirect('/sign_in')
+
+    get '/sign_in' => 'clearance/sessions#new', :as => 'sign_in'
+    delete '/sign_out' => 'clearance/sessions#destroy', :as => 'sign_out'
+
+    if Clearance.configuration.allow_sign_up?
+      get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
+    end
   end
